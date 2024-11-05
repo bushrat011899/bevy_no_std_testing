f82dc51fe2c6ae96088c2ba6a9946fb77d0c9920
heydocode <169077757+heydocode@users.noreply.github.com>
2024-11-05 22:02:34 +0100

    Fixed depedencies
    Current bevy fork has way more features & internal crates which support no-std, so why not extend the no-std bevy capabalities?

--- crates/game/src/lib.rs
+++ crates/game/src/lib.rs
@@ -1,9 +1,10 @@
 #![no_std]
 #![allow(unused_imports)]
 
-use bevy_ptr;
 use bevy_utils;
-use bevy_mikktspace;
+use bevy_tasks;
+use bevy_ecs;
+use bevy_app;
 
 pub fn entry() {
     log::info!("Hello World!");
